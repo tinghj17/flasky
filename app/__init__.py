da98814219723cf5344cd71ffe15d512f6d85bd2
tinghj17 <tinghj17@gmail.com>
2022-05-11 15:44:18 -0700

    moves migrate

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ import os
 
 # create the object
 db = SQLAlchemy()
-migrate = Migrate()
 load_dotenv()
 
 from .models.cars import Car
@@ -19,6 +18,7 @@ def create_app(testing = None):
     # __name__ stores the name of the module we're in
     # creating a new flask object
     app = Flask(__name__)
+    migrate = Migrate(app, db, include_schemas=True)
 
     # put configuration, for legacy.
     app.config["SQLALCHEMY_TEACK_MODIFICATIONS"] = False
